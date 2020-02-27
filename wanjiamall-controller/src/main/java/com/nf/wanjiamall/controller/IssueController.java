3edde4f20db7071e054c40388fc1707565a1c4de
GD <1841550221@qq.com>
2020-02-27 23:14:01 +0800

    issue表的第三次修改

--- wanjiamall-controller/src/main/java/com/nf/wanjiamall/controller/IssueController.java
+++ wanjiamall-controller/src/main/java/com/nf/wanjiamall/controller/IssueController.java
@@ -55,7 +55,7 @@ public class IssueController {
     })
 
     @PostMapping("/issue")
-    @ApiOperation("添加常见问题表,传一个issue集合")
+    @ApiOperation("添加常见问题表,传一个issue表集合")
     public ResponseVo issueInsert(IssueEntity issueEntity) {
        boolean result = issueService.issueInsert(issueEntity);
        if(result != false){
