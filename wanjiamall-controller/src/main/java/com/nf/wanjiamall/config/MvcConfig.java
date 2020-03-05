cf647c380a1a6366d77fa03f515976680daa7d51
sam <2562225391@qq.com>
2020-03-06 00:32:01 +0800

    wsx 修改lrc

--- wanjiamall-controller/src/main/java/com/nf/wanjiamall/config/MvcConfig.java
+++ wanjiamall-controller/src/main/java/com/nf/wanjiamall/config/MvcConfig.java
@@ -4,8 +4,13 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 /**
  * mvc全局配置
@@ -25,5 +30,11 @@ public class MvcConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "TRACE")
                 .maxAge(3600);
     }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatter(new org.springframework.format.datetime.DateFormatter("yyyy-MM-dd"));
+    }
+
 
 }
