db849670b0f9b2551edda769d8cc86bf4c4db582
GD <1841550221@qq.com>
2020-03-06 23:54:34 +0800

    yx 完成了微信端的收藏服务

--- wanjiamall-dao/src/main/java/com/nf/wanjiamall/dao/CollectDao.java
+++ wanjiamall-dao/src/main/java/com/nf/wanjiamall/dao/CollectDao.java
@@ -2,6 +2,7 @@ package com.nf.wanjiamall.dao;
 
 import com.nf.wanjiamall.entity.CollectEntity;
 import com.nf.wanjiamall.entity.GoodsEntity;
+import com.nf.wanjiamall.entity.IssueEntity;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -12,4 +13,9 @@ import java.util.List;
 public interface CollectDao{
     List<CollectEntity> getCollectList(@Param("pageNum") Integer pageNum, @Param("pageSize") Integer pageSize,
                                        @Param("collectEntity") CollectEntity collectEntity);
+    List<CollectEntity> getCollectByUserId(@Param("pageNum") Integer pageNum, @Param("pageSize") Integer pageSize,
+                                       @Param("userId") Integer userId);
+    CollectEntity getCollectInsertAndDelete(@Param("userId") Integer userId,@Param("goodsId") Integer goodsId);
+    int collectInsert(@Param("userId") Integer userId,@Param("goodsId") Integer goodsId);
+    int collectDelete(@Param("userId") Integer userId,@Param("goodsId") Integer goodsId);
 }
--- wanjiamall-service/src/main/java/com/nf/wanjiamall/service/CollectService.java
+++ wanjiamall-service/src/main/java/com/nf/wanjiamall/service/CollectService.java
@@ -1,12 +1,14 @@
 package com.nf.wanjiamall.service;
 
 import com.nf.wanjiamall.entity.CollectEntity;
-import com.nf.wanjiamall.entity.GoodsEntity;
-import org.apache.ibatis.annotations.Param;
 
 /**
  * @author 南八
  */
 public interface CollectService {
    Object getCollectList(Integer pageNum,Integer pageSize,CollectEntity collectEntity);
+   Object getCollectByUserId(Integer pageNum,Integer pageSize,Integer userId);
+   Object getCollectInsertAndDelete(Integer userId,Integer goodsId);
+   Object collectInsert(Integer userId,Integer goodsId);
+   Object collectDelete( Integer userId,Integer goodsId);
 }
