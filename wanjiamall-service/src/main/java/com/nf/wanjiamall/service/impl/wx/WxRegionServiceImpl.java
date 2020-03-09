f3c29d49d0623fe006f952ba303dec6983017d0e
hai sao <775190041@qq.com>
2020-03-09 22:06:34 +0800

    完成了行政区域查询

---
+++ wanjiamall-service/src/main/java/com/nf/wanjiamall/service/impl/wx/WxRegionServiceImpl.java
@@ -0,0 +1,35 @@
+package com.nf.wanjiamall.service.impl.wx;
+
+import com.nf.wanjiamall.dao.RegionDao;
+import com.nf.wanjiamall.entity.RegionEntity;
+import com.nf.wanjiamall.service.wx.WxRegionService;
+import com.nf.wanjiamall.utils.ResponseUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 行政区域表 实现类
+ */
+@Service
+@Slf4j
+public class WxRegionServiceImpl implements WxRegionService {
+
+   @Autowired
+   private RegionDao regionDao;
+
+    @Override
+    public Object getRegionProvinceQuery() {
+        return ResponseUtil.ok(regionDao.getRegionProvinceQuery());
+    }
+
+    @Override
+    public Object getRegionPidCityQuery(Integer type, Integer pId) {
+       return  ResponseUtil.ok(regionDao.getRegionPidCityQuery(type,pId));
+    }
+}
