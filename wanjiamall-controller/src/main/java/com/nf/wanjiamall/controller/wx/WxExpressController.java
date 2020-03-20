91849ee815717ad87be0d215423c4975b0c6e90c
sam <2562225391@qq.com>
2020-03-20 15:46:04 +0800

    wsx 修改

--- wanjiamall-controller/src/main/java/com/nf/wanjiamall/controller/wx/WxExpressController.java
+++ wanjiamall-controller/src/main/java/com/nf/wanjiamall/controller/wx/WxExpressController.java
@@ -2,21 +2,28 @@ package com.nf.wanjiamall.controller.wx;
 
 import com.nf.wanjiamall.entity.ExpressInfo;
 import com.nf.wanjiamall.service.impl.ExpressService;
+import com.nf.wanjiamall.utils.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/wx")
 public class WxExpressController {
     @Autowired
     private ExpressService expressService;
     @RequestMapping("/express/{id}")
-    @ResponseBody
     public ExpressInfo getExpress(@PathVariable Integer id)  {
         return expressService.getExpressInfo("ZTO","123456");
+    }
+    /**
+     * 查询物流公司
+     *
+     * @return
+     */
+    @GetMapping("/channel")
+
+    public Object channel() {
+        return ResponseUtil.ok(expressService.getVendors());
     }
 }
