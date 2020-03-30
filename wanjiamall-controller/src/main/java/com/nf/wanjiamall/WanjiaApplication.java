7cc86ffed77b394b11a2abbb4f77b15d5b851281
Ilario <568372341@qq.com>
2020-03-30 12:53:16 +0800

    1

--- wanjiamall-controller/src/main/java/com/nf/wanjiamall/WanjiaApplication.java
+++ wanjiamall-controller/src/main/java/com/nf/wanjiamall/WanjiaApplication.java
@@ -11,5 +11,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class WanjiaApplication {
     public static void main(String[] args) {
        SpringApplication.run(WanjiaApplication.class, args);
+
+
     }
 }
